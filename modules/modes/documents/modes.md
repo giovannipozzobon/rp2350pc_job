c3d2b5f065a5fa220ab54cf7a98cae5b6ee7a5d6
Paul Robson <paul@robsons.org.uk>
2025-07-07 18:19:54 +0100

    Skeleton module

--- modules/modes/documents/modes.md
+++ modules/modes/documents/modes.md
@@ -21,7 +21,7 @@ One is the generation of the horizontal line, which is the same as the DVI Modul
 
 The second is the vertical resolution. This can be anything from 16 to 480 in steps of 16 ; the scanlines are adjusted to fit on the screen, and the display is vertically centred. So a 320x192 display would be 384 display lines (2 scan lines per line) with 48 blank lines above and below.
 
-The third is the buffering. On initialisation the address and size of the frame buffer is provided, along with the mode. The module works out how many buffers you have on this mode. These can be selected as follows.
+The third is the buffering. After initialisation the address and size of the frame buffer is provided, along with the mode. The module works out how many buffers you have on this mode. These can be selected as follows.
 
 1) The user manually selects which buffer is to be displayed.
 2) On sync command, the buffers 'swap'. There is a buffer that is currently being drawn to, and one being displayed, when the buffers swap the new buffer being drawn to is either (a) cleared to a solid colour (b) the current display buffer is copied to it (c) another buffer is copied to it (d) a user routine is run to do ... whatever you want to it. This latter is in an interrupt so needs to be relatively short.
--- modules/modes/include/modes_module.h
+++ modules/modes/include/modes_module.h
@@ -8,3 +8,40 @@
 #ifdef LOCALS
 #endif
 
+//
+//      Horizontal Resolutions
+//
+#define     VMODE_HRES_160      (0x0000)
+#define     VMODE_HRES_320      (0x0001)
+#define     VMODE_HRES_640      (0x0002)
+//
+//      Pixels per Byte
+//
+#define     VMODE_PPB_1         (0x0010)
+#define     VMODE_PPB_2         (0x0020)
+#define     VMODE_PPB_4         (0x0040)
+#define     VMODE_PPB_8         (0x0080)
+//
+//      Colour enabled flag
+//
+#define     VMODE_USE_COLOUR    (0x0100)
+//
+//      Variable vertical resolution
+//
+#define     VMODE_VRES(l)       (((l) >> 4) << 9)
+//
+//      Currently supported modes.
+//
+#define     MODE_640_480_256    (VMODE_HRES_640|VMODE_USE_COLOUR|VMODE_PPB_1|VMODE_VRES(480))
+#define     MODE_640_480_16     (VMODE_HRES_640|VMODE_USE_COLOUR|VMODE_PPB_2|VMODE_VRES(480))
+#define     MODE_640_480_MONO4  (VMODE_HRES_640|VMODE_PPB_4|VMODE_VRES(480))
+#define     MODE_640_480_MONO1  (VMODE_HRES_640|VMODE_PPB_8|VMODE_VRES(480))
+
+#define     MODE_320_240_256    (VMODE_HRES_320|VMODE_USE_COLOUR|VMODE_PPB_1|VMODE_VRES(240))
+#define     MODE_160_240_256    (VMODE_HRES_160|VMODE_USE_COLOUR|VMODE_PPB_1|VMODE_VRES(240))
+#define     MODE_320_200_256    (VMODE_HRES_320|VMODE_USE_COLOUR|VMODE_PPB_1|VMODE_VRES(200))
+#define     MODE_160_200_256    (VMODE_HRES_160|VMODE_USE_COLOUR|VMODE_PPB_1|VMODE_VRES(200))
+
+void MODInitialise(void)
+void MODSetFrameBuffer(uint8_t *frameBuffer,uint32_t size);
+void MODSetMode(uint32_t mode);
