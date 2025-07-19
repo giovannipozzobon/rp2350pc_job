15887d3268255cf04795e05ec785cdd35bfee05e
Paul Robson <paul@robsons.org.uk>
2025-07-19 08:57:53 +0100

    Double buffer/redraw working

--- TODO.md
+++ TODO.md
@@ -1,7 +1,6 @@
 # TODO list
 
 ## Sprite module
-- make it double buffered (e.g. memcpy on VSync) and draw stuff on the display
 - make it moveable objects 
 - increase the moveable objects and see how it holds up.
 
--- modules/sprites/app/main.c
+++ modules/sprites/app/main.c
@@ -2,6 +2,9 @@
 
 void demo();
 
+static void ListDirectory(void);
+static void ListFile(void);
+
 uint8_t vRAM[640*480];
 
 int MAINPROGRAM(int argc,char *argv[]) {
@@ -10,6 +13,8 @@ int MAINPROGRAM(int argc,char *argv[]) {
     GFXDraw(Mode,MODE_320_240_256,0);                                               // Set mode. This has 2 buffers, which will be the back and front.
     LOG("%d\n",vi.bufferCount);
     INPInitialise();
+
+    vi.displaySurface = vi.buffer[1];
 
     for (int i = 0; i < 80;i += 2) {                                                // Draw *something* as a background :)
         GFXDraw(RawColour,rand() & 0xFF,0);
@@ -20,22 +25,58 @@ int MAINPROGRAM(int argc,char *argv[]) {
     while (COMAppRunning()) { 
         int16_t k = INPGetKey();  
         if (k != 0) LOG("Key %d",k);
-
+        if (toupper(k) == 'F') ListFile();
+        if (toupper(k) == 'D') ListDirectory();
         INPUpdate();
         USBUpdate();                                                                // Update USB (in this case keyboard messages)
         YIELD();                         
     }
 }
 
+static int vPos = 0;
+
 void demo() {
     verticalSyncOccurred = false;                                                   // Forces wait one frame.
     while (COMAppRunning()) {
         if (verticalSyncOccurred) {
-            for (int i = 0;i < 100000*100;i++) {
-                vRAM[random() % (320*20)] = random();
+            vPos = (vPos+1) & 0x7F;
+            memcpy(vi.displaySurface,vi.drawSurface,vi.bufferSize);
+            for (int i = 0;i < 10000;i++) {
+                vi.displaySurface[random() % (320*20)+vPos*320] = random();
             }
             verticalSyncOccurred = false;
         }
     }
 }
 
+/**
+ * @brief      List the root directory
+ */
+static void ListDirectory(void) {
+    char *path = ""; 
+    int32_t error,handle = FSOpenDirectory(path);
+    if (handle >= 0) {
+        FSOBJECTINFO fInfo;
+        while (error = FSReadDirectory(handle,&fInfo),error == 0) {
+            LOG("%c %-8d %s",fInfo.isDirectory ? 'D':'.',fInfo.size,fInfo.name);
+        }
+        if (error != FSERR_EOF) LOG("Read error : %d",error);
+        FSCloseDirectory(handle);        
+    }
+}
+
+/**
+ * @brief      List part of a file on the USB key.
+ */
+static void ListFile(void) {
+    int32_t error,handle = FSOpen("loops.bsc");
+    if (handle == 0) {
+        error = FSSeek(handle,12);
+        //LOG("Seek result %d",error);
+        //LOG("Tell result %d",FSTell(handle));
+        char buffer[129];
+        error = FSRead(handle,buffer,128);buffer[128] = '\0';
+        LOG("Read %d : [%s]",error,buffer);
+        error = FSClose(handle);
+    }
+}
