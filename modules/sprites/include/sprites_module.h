324de1a6f4c42a6195877f91feb9391f5f7fb3ee
Paul Robson <paul@robsons.org.uk>
2025-07-27 20:51:33 +0100

    Sprites skeleton

---
+++ modules/sprites/include/sprites_module.h
@@ -0,0 +1,22 @@
+// *******************************************************************************************
+// *******************************************************************************************
+//
+//      Name :      sprites_module.h
+//      Purpose :   Sprite external include
+//      Date :      27th July 2025
+//      Author :    Paul Robson (paul@robsons.org.uk)
+//
+// *******************************************************************************************
+// *******************************************************************************************
+
+#pragma once
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "common_module.h"
+#include "usb_module.h"
+#include "dvi_module.h"
+#include "modes_module.h"
